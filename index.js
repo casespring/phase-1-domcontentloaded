789fae660cb24f97fb84b5f8e2c656f337152c5c
Marco Upia <markremembers@gmail.com>
2023-11-02 15:49:29 -0400

    Dom loader

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", () => {
+    let text = document.getElementById('text')
+    text.textContent = "The is really cool!"
+    console.log("The DOM as loaded");
+});
+console.log("This fires befor the DOM is Loaded!");
